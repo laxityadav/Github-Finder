d7c321fb5f4c45283de6d33885f367120bebab88
laxit yadav <44118317+laxityadav@users.noreply.github.com>
2021-02-06 19:59:17 +0530

    Update githubReducer.js

--- src/context/github/githubReducer.js
+++ src/context/github/githubReducer.js
@@ -3,7 +3,7 @@ import { SEARCH_USERS, SET_LOADING, CLEAR_USERS, GET_USER, GET_REPOS } from '../
 export default (state, action) => {
     switch (action.type) {
         case SEARCH_USERS: return {
-            ...state,           //it would copy whatever is there in the current state
+            ...state,           //it would copy whatever is there in the current state(state is immutable so that we have to make a copy of whole state object and return it)
             users: action.payload,  //copying the data coming from GithubState
             loading: false
         };
